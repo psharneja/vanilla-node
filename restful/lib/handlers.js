bf0e0a01c2b8dd98f0a146deabaf379fe4667ce0
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2019-11-05 13:18:03 +0530

    initial index poc

--- restful/lib/handlers.js
+++ restful/lib/handlers.js
@@ -9,6 +9,34 @@ var config = require("./config");
 //define the handlers
 var handlers = {};
 
+
+
+/**
+ * 
+ * HTML HANDLERS SECTION
+ */
+handlers.index = function(data, callback) {
+    //reject request that isn't GET
+    if (data.method == 'get') {
+        //read in a template as a string
+        helpers.getTemplate('index', function(err, str) {
+            if (!err && str) {
+                callback(200, str, 'html');
+            } else {
+                callback(500, undefined, 'html');
+            }
+        });
+
+    } else {
+        callback(405, undefined, 'html');
+    }
+}
+
+
+/**
+ * 
+ * Everything below is JSON API HANDLER
+ */
 // users
 handlers.users = function(data, callback) {
     var acceptableMethods = ["post", "get", "put", "delete"];
--- restful/lib/server.js
+++ restful/lib/server.js
@@ -8,17 +8,14 @@ var url = require("url");
 var StringDecoder = require("string_decoder").StringDecoder;
 var config = require("./config");
 var fs = require("fs");
-var handlers = require('./handlers');
-var helpers = require('./helpers');
-var path = require('path');
-var util = require('util');
-var debug = util.debuglog('server')
+var handlers = require("./handlers");
+var helpers = require("./helpers");
+var path = require("path");
+var util = require("util");
+var debug = util.debuglog("server");
 
 // instantiate server module object
 var server = {};
-
-
-
 
 //@TODO get rid of this
 // helpers.sendTwilioSms('9711838993', 'Hello', function(err) {
@@ -30,18 +27,17 @@ server.httpServer = http.createServer(function(req, res) {
     server.unifiedServer(req, res);
 });
 
-
-
 //instantiate https server
 server.httpsServerOptions = {
     key: fs.readFileSync(path.join(__dirname, "/../https/key.pem")),
     cert: fs.readFileSync(path.join(__dirname, "/../https/cert.pem"))
 };
-server.httpsServer = https.createServer(server.httpsServerOptions, function(req, res) {
+server.httpsServer = https.createServer(server.httpsServerOptions, function(
+    req,
+    res
+) {
     server.unifiedServer(req, res);
 });
-
-
 
 // all the server logic for both http n https
 server.unifiedServer = function(req, res) {
@@ -86,29 +82,43 @@ server.unifiedServer = function(req, res) {
         };
 
         // route the request to the handler specified in the router
-        chosenHandler(data, function(statusCode, payload) {
+        chosenHandler(data, function(statusCode, payload, contentType) {
+            //determine the type of response, fallback to json
+            contentType = typeof contentType == "string" ? contentType : "json";
             // use the status code callback or default 200
             statusCode = typeof statusCode == "number" ? statusCode : 200;
 
             //use the payload callback or define empty object
-            payload = typeof payload == "object" ? payload : {};
 
-            // convert the payload to a string
-            var payloadString = JSON.stringify(payload);
+            // send response parts that are content specific
+            var payloadString = "";
+            if (contentType == "json") {
+                res.setHeader("Content-Type", "application/json");
+                payload = typeof payload == "object" ? payload : {};
+                payloadString = JSON.stringify(payload);
+            }
 
-            // return response
-            res.setHeader("Content-Type", "application/json");
+            if (contentType == "html") {
+                res.setHeader("Content-Type", "text/html");
+                payloadString = typeof payload == "string" ? payload : "";
+            }
+
+            //send response parts which are common
             res.writeHead(statusCode);
-
-            // send response
             res.end(payloadString);
 
             // log the request path
             // if response is 220, print green otherwise red
             if (statusCode == 200) {
-                debug('\x1b[32m%s\x1b[0m', method.toUpperCase() + ' /' + trimmedPath + ' ' + statusCode);
+                debug(
+                    "\x1b[32m%s\x1b[0m",
+                    method.toUpperCase() + " /" + trimmedPath + " " + statusCode
+                );
             } else {
-                debug('\x1b[31m%s\x1b[0m', method.toUpperCase() + ' /' + trimmedPath + ' ' + statusCode);
+                debug(
+                    "\x1b[31m%s\x1b[0m",
+                    method.toUpperCase() + " /" + trimmedPath + " " + statusCode
+                );
             }
         });
     });
@@ -116,26 +126,39 @@ server.unifiedServer = function(req, res) {
 
 // define a request router
 server.router = {
+    "": handlers.index,
+    "account/create": handlers.accountCreate,
+    "account/edit": handlers.accountEdit,
+    "account/deleted": handlers.accountDeleted,
+    "session/create": handlers.sessionCreate,
+    "session/deleted": handlers.sessionDeleted,
+    "checks/all": handlers.checksList,
+    "checks/create": handlers.checksCreate,
+    "checks/edit": handlers.checksEdit,
     ping: handlers.ping,
-    users: handlers.users,
-    tokens: handlers.tokens,
-    checks: handlers.checks
+    "api/users": handlers.users,
+    "api/tokens": handlers.tokens,
+    "api/checks": handlers.checks
 };
 //init method
 server.init = function() {
     //start the http server
     //start the server and have it listen to port 3000
     server.httpServer.listen(config.httpPort, function() {
-
-        console.log('\x1b[36m%s\x1b[0m', "the server is listening on port " + config.httpPort);
+        console.log(
+            "\x1b[36m%s\x1b[0m",
+            "the server is listening on port " + config.httpPort
+        );
     });
     //start the https server
     //start https server
     server.httpsServer.listen(config.httpsPort, function() {
-        console.log('\x1b[35m%s\x1b[0m', "the server is listening on port " + config.httpsPort);
+        console.log(
+            "\x1b[35m%s\x1b[0m",
+            "the server is listening on port " + config.httpsPort
+        );
     });
-}
-
+};
 
 //export the module
 module.exports = server;
